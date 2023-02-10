d5e2fd622473853d993e0a32917f64a68383d8f3
rafael <rafaelcnunes@icloud.com>
2023-02-10 02:17:16 -0300

    added TheTypography and spec test

---
+++ src/components/atoms/TheTypography.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="the_typography">
+    <div
+      :class="[
+        `font-size-${textSize}`,
+        `font-size-display-${displaySize}`,
+        `font-weight-${weight}`,
+      ]"
+    >
+      {{ text }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheTypography",
+  props: {
+    textSize: {
+      type: String,
+      default: "",
+    },
+    displaySize: {
+      type: String,
+      default: "",
+    },
+    weight: {
+      type: String,
+      default: "regular",
+    },
+    text: {
+      type: String,
+      default: "Texto padrão",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+.the_typography {
+  font-family: "Public Sans";
+}
+
+.font-size {
+  &-xs {
+    font-size: 12px;
+  }
+
+  &-sm {
+    font-size: 14px;
+  }
+
+  &-md {
+    font-size: 16px;
+  }
+
+  &-lg {
+    font-size: 18px;
+  }
+
+  &-display-xs {
+    font-size: 24px;
+  }
+
+  &-display-sm {
+    font-size: 30px;
+  }
+
+  &-display-md {
+    font-size: 36px;
+  }
+
+  &-display-lg {
+    font-size: 48px;
+  }
+
+  &-display-xl {
+    font-size: 60px;
+  }
+
+  &-display-2xl {
+    font-size: 72px;
+  }
+}
+
+.font-weight {
+  &-regular {
+    font-weight: 500;
+  }
+
+  &-medium {
+    font-weight: 400;
+  }
+
+  &-semibold {
+    font-weight: 600;
+  }
+
+  &-bold {
+    font-weight: 700;
+  }
+}
+</style>
---
+++ tests/unit/the_typography.spec.js
@@ -0,0 +1,44 @@
+import { shallowMount } from "@vue/test-utils";
+import TheTypography from "@/components/atoms/TheTypography.vue";
+
+describe("TheTypography.vue", () => {
+  it("renders the correct text", () => {
+    const text = "Hello TheTypography Test Text";
+    const wrapper = shallowMount(TheTypography, {
+      propsData: {
+        text: text,
+      },
+    });
+    expect(wrapper.text()).toMatch(text);
+  });
+
+  it("renders the correct font size", () => {
+    const size = "sm";
+    const wrapper = shallowMount(TheTypography, {
+      propsData: {
+        textSize: size,
+      },
+    });
+    expect(wrapper.props().textSize).toBe(size);
+  });
+
+  it("renders the correct display size", () => {
+    const size = "lg";
+    const wrapper = shallowMount(TheTypography, {
+      propsData: {
+        displaySize: size,
+      },
+    });
+    expect(wrapper.props().displaySize).toBe(size);
+  });
+
+  it("renders the correct font weight", () => {
+    const weight = "bold";
+    const wrapper = shallowMount(TheTypography, {
+      propsData: {
+        weight: weight,
+      },
+    });
+    expect(wrapper.props().weight).toBe(weight);
+  });
+});
