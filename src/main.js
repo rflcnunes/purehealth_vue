dbdb5c2d51f4b2c305686d53162c0c9d28d78652
rafael <rafaelcnunes@icloud.com>
2023-02-10 22:28:31 -0300

    added vue mq

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueMq from "vue-mq";
+
+Vue.use(VueMq, {
+  breakpoints: {
+    mobile: 767,
+    tablet: 1200,
+    desktop: Infinity,
+  },
+
+  defaultBreakpoint: "tablet",
+});
 
 Vue.config.productionTip = false;
 
