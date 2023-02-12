715d0356d58f7b33af7c4718d081fd4ebfd3f08f
rafael <rafaelcnunes@icloud.com>
2023-02-12 01:39:00 -0300

    added TheCheckbox

---
+++ src/components/atoms/TheCheckbox.vue
@@ -0,0 +1,97 @@
+<template>
+  <div
+    id="the_checkbox"
+    @click="clickDisabled"
+    :class="{
+      checked: checked && !isDisabled,
+      unchecked: !checked && !isDisabled,
+      disabled: isDisabled,
+    }"
+  >
+    <span v-if="checked" class="material-icons icon">done</span>
+  </div>
+</template>
+
+<script>
+import { required } from "vuelidate/lib/validators";
+
+export default {
+  name: "TheCheckbox",
+  props: {
+    isRequired: {
+      type: Boolean,
+      default: false,
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      checked: false,
+    };
+  },
+  computed: {
+    clickDisabled() {
+      if (!this.isDisabled) {
+        return () => {
+          this.checked = !this.checked;
+        };
+      }
+      return () => {};
+    },
+  },
+  validations: {
+    checked: {
+      required: required,
+    },
+  },
+  methods: {
+    validate() {
+      this.$v.$touch();
+      if (!this.$v.$error) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#the_checkbox {
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.unchecked {
+  border: 1px solid $blue-gray;
+}
+
+.unchecked:hover {
+  box-shadow: $input-blue-shadow;
+}
+
+.checked {
+  color: #fff;
+  background-color: $purple;
+  border: 1px solid $purple;
+}
+
+.checked:hover {
+  box-shadow: none;
+}
+
+.disabled {
+  border: 1px solid $disabled-gray;
+  box-shadow: 0;
+  outline: none;
+}
+</style>
