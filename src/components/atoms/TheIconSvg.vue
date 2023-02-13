0b6412f0be7eb885b99658f52565cfb220a5f9a6
rafael <rafaelcnunes@icloud.com>
2023-02-13 04:14:50 -0300

    added TheIconSvg

---
+++ src/components/atoms/TheIconSvg.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <img :class="[`icon-${size}`]" :src="iconUrl" alt="icon" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheIconSvg",
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      default: "md",
+    },
+  },
+  computed: {
+    iconUrl() {
+      return require(`@/assets/original/${this.icon}.svg`);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  &-sm {
+    width: 24px;
+  }
+
+  &-md {
+    width: 28px;
+  }
+
+  &-lg {
+    width: 30px;
+  }
+
+  &-xl {
+    width: 32px;
+  }
+}
+</style>
