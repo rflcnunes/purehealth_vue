634662df1738e32f5fa2dc3ff147b9d32fed9e92
rafael <rafaelcnunes@icloud.com>
2023-02-13 04:19:32 -0300

    added TheSocialMediaButton

---
+++ src/components/atoms/TheSocialMediaButton.vue
@@ -0,0 +1,88 @@
+<template>
+  <button
+    class="btn"
+    @click="$emit('input', $event)"
+    type="submit"
+    :class="[`btn-${size}`]"
+    :disabled="isDisabled"
+  >
+    <TheIconSvg size="sm" :icon="icon" />
+    <TheTypography :text="label" :textSize="size" />
+  </button>
+</template>
+
+<script>
+import TheIconSvg from "./TheIconSvg.vue";
+import TheTypography from "./TheTypography.vue";
+
+export default {
+  name: "TheSocialMediaButton",
+  components: {
+    TheTypography,
+    TheIconSvg,
+  },
+  data() {
+    return {
+      value: "",
+    };
+  },
+  props: {
+    isDisabled: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+    type: {
+      type: String,
+      default: "default",
+    },
+    icon: {
+      type: String,
+      default: "",
+    },
+    size: {
+      type: String,
+      default: "md",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  border-radius: 8px;
+  margin: 2px;
+  border: 0;
+  background-color: $light-bg;
+  display: grid;
+  grid-template-columns: 0.2fr auto;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  &:hover {
+    background-color: $disabled-gray;
+  }
+}
+
+.btn {
+  &-sm {
+    padding: 6px 16px;
+  }
+
+  &-md {
+    padding: 10px 20px;
+  }
+
+  &-lg {
+    padding: 14px 24px;
+  }
+
+  &-xl {
+    padding: 16px 32px;
+  }
+}
+</style>
