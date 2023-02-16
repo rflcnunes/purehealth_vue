14eb44d3495adf1fb3fabed1fb9fd57ce3049d1b
rafael <rafaelcnunes@icloud.com>
2023-02-15 21:53:09 -0300

    remove route

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import ShippingView from "../views/ShippingView.vue";
 
 Vue.use(VueRouter);
 
@@ -19,11 +18,6 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-  {
-    path: "/shipping",
-    name: "shipping",
-    component: ShippingView,
   },
 ];
 
