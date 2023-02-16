ee1596c4926e4366af3aad95c8738e1cdf33a926
rafael <rafaelcnunes@icloud.com>
2023-02-15 22:28:16 -0300

    added AboutView

--- src/components/atoms/TheIconSvg.vue
+++ src/components/atoms/TheIconSvg.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
-    <img :class="[`icon-${size}`]" :src="iconUrl" alt="icon" />
+  <div :class="`glassmorphism icon-${size}`">
+    <div class="icon-wrapper">
+      <img :src="iconUrl" alt="icon" />
+    </div>
   </div>
 </template>
-
 <script>
 export default {
   name: "TheIconSvg",
@@ -24,9 +25,28 @@ export default {
   },
 };
 </script>
-
 <style lang="scss" scoped>
-.icon {
+.glassmorphism {
+  background: rgba(255, 255, 255, 0.1);
+  border-radius: 10px;
+  backdrop-filter: blur(5px);
+  padding: 10px;
+  transition: all 0.3s ease-in-out;
+
+  &:hover {
+    background: rgba(255, 255, 255, 0.2);
+  }
+
+  .icon-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  img {
+    max-width: 100%;
+  }
+
   &-sm {
     width: 24px;
   }
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,62 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <TheTypography
+      class="color_text"
+      text="Thanks for testing!"
+      displaySize="sm"
+    />
+    <TheTypography
+      class="color_text"
+      text="Tell me what you think"
+      displaySize="sm"
+    />
+    <div class="actions">
+      <a href="https://github.com/rflcnunes" target="_blank">
+        <TheIconSvg size="2xl" icon="github" />
+      </a>
+
+      <a href="https://www.linkedin.com/in/rflcstnunes/" target="_blank">
+        <TheIconSvg :glassmorphism="true" size="2xl" icon="linkedin" />
+      </a>
+    </div>
   </div>
 </template>
+
+<script>
+import TheIconSvg from "@/components/atoms/TheIconSvg.vue";
+import TheTypography from "@/components/atoms/TheTypography.vue";
+
+export default {
+  components: {
+    TheTypography,
+    TheIconSvg,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.about {
+  background-color: $primary-gray;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  .color_text {
+    color: $light-gray;
+  }
+
+  .actions {
+    width: 100%;
+    display: grid;
+    justify-content: space-between;
+    align-items: center;
+    justify-content: center;
+    grid-auto-flow: column;
+    grid-gap: 10px;
+    padding: 100px 0 0 0;
+  }
+}
+</style>
